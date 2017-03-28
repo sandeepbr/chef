b8543339b3d758f3c9880b06c214ef93afcc54e7
sandeepbr <bhimanapatisandeep@gmail.com>
2017-03-28 01:37:00 +0000

    adding cookbook custom_apache

---
+++ cookbooks/custom_apache/metadata.rb
@@ -0,0 +1,7 @@
+name             'custom_apache'
+maintainer       'sandeep company'
+maintainer_email 'bhimanapatisandeep@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures custom_apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ cookbooks/custom_apache/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: custom_apache
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package 'httpd' do
+action :install
+end
+
+service 'httpd' do
+action [:enable, :start]
+end
